9145475349224cbb01785251e8c8ed24d3ae9c36
Hebron <henronenyeww@gmail.com>
2026-03-04 20:42:55 +0300

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import AuthProvider from "./auths/AuthContext.jsx";
-import { BrowserRouter } from 'react-router-dom'
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import AuthProvider from "./auths/AuthContext.jsx"; // default export
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>                    
-      <BrowserRouter>                 
-        <App />                       
-      </BrowserRouter>
+    <AuthProvider>
+      <App />
     </AuthProvider>
   </React.StrictMode>
-)
+);
