e23b062a556b6ad59fe4727c1dec433b7e36cd2b
Hebron <henronenyeww@gmail.com>
2026-03-04 20:09:06 +0300

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthProvider  from "./auths/AuthContext"
+import AuthProvider from "./auths/AuthContext.jsx";
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 
